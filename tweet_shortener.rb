393e6787a2ce6626ae81e0160ae4519c78ab1374
Jason Basuil <basuilj@gmail.com>
2018-12-27 20:26:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,16 +2,16 @@ def dictionary
   #contains the words-to-be-substituted and their substitutes
  
   dictionary = {
-    :hello => "hi",
-    :to => "2",
-    :two => "2",
-    :too => "2",
-    :for => "4",
-    :four => "4",
-    :be => "b",
-    :you => "u",
-    :at => "@",
-    :and => "&"
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
   }
 end 
   
